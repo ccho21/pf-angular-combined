a3ef3281a11e49af3ac12f838523d7fddc075b86
Changsoo cho <changsoo.charles.cho@gmail.com>
2022-04-26 23:25:37 -0400

    updated

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use('/api/upload', require('./routes/api/upload'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
